7f1555bf77c403821f67a3b440ddeb26a4f03b1f
pafosz <124775152+pafosz@users.noreply.github.com>
2023-10-08 23:31:31 +0400

    found the value of the dollar

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import os
+from bs4 import BeautifulSoup
 import requests
+import re
+from re import sub
+from decimal import Decimal
+import io
+from datetime import datetime
+
+url = 'https://www.cbr.ru/currency_base/daily/'
+html_text = requests.get(url).text
+soup = BeautifulSoup(html_text, 'lxml')
+table_wrapper = soup.find('div', class_ = 'table-wrapper')
+table = table_wrapper.find('div', class_ = 'table')
+data = table.find('table', class_ = 'data')
+tr = data.find_all('tr')[14]
+td = tr.find_all('td')[4].text
+print(td)
+# Доллары появились начиная с 1996г
