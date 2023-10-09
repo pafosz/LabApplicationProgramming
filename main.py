deb2c611cf731955e3cbafe40952416fc8bac0a2
pafosz <124775152+pafosz@users.noreply.github.com>
2023-10-10 00:17:44 +0400

    added time cycles

--- main.py
+++ main.py
@@ -7,13 +7,25 @@ from decimal import Decimal
 import io
 from datetime import datetime
 
-url = 'https://www.cbr.ru/currency_base/daily/'
-html_text = requests.get(url).text
-soup = BeautifulSoup(html_text, 'lxml')
-table_wrapper = soup.find('div', class_ = 'table-wrapper')
-table = table_wrapper.find('div', class_ = 'table')
-data = table.find('table', class_ = 'data')
-tr = data.find_all('tr')[14]
-td = tr.find_all('td')[4].text
-print(td)
+url = range('https://www.cbr.ru/currency_base/daily/?UniDbQuery.Posted=True&UniDbQuery.To={day}.{month}.{year}')
+# html_text = requests.get(url).text
+# soup = BeautifulSoup(html_text, 'lxml')
+
+# block_usd = soup.find('table', class_ = 'data').find_all('tr')[14]
+# course = block_usd.find_all('td')[4].text
+
+# date = soup.find('button', class_ = 'datepicker-filter_button').text
+# #print(date)
+days = range(1, 31)
+for day in days:
+    print(day+1)
+now_year =  datetime.now().year
+manth = datetime.now().month
+years = range(1996, now_year)
+for year in years:
+    print(year+1)
+
+     
+
+
 # Доллары появились начиная с 1996г
